1d11c6ba6069a8ff3bd44203b642bf0cdf8d664b
rhinofld <rhinofld@f2db2696-81b8-11de-bd06-fbfebf6038a7>
2009-11-05 13:50:12 +0000

    增加webean和登录过滤器

--- trunk/core/src/main/java/com/bizcreator/core/session/UserManager.java
+++ trunk/core/src/main/java/com/bizcreator/core/session/UserManager.java
@@ -1,9 +1,12 @@
 package com.bizcreator.core.session;
 
+import com.bizcreator.core.LoginContext;
 import com.bizcreator.core.dao.BasicDao;
 
 public interface UserManager extends BasicDao {
 
 	public final static String NAME = "core.userMgr";
+	
+	public LoginContext authenticate(String username, String password);
 	
 }
